e05bddf76938cf80335ba18ab4c35e3d3263ee6d
DennisCarrera <denniscarrera@live.com.mx>
2019-02-05 08:23:27 -0600

    Creacion de personajes y puntajes

--- src/reto3/Reto3.java
+++ src/reto3/Reto3.java
@@ -6,6 +6,7 @@
 package reto3;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -17,14 +18,21 @@ public class Reto3 {
     /**
      * @param args the command line arguments
      */
-    
-    
-    
     public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in)); 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int puntaje;
         Constructor personaje1 = new Constructor();
         Mago personaje2 = new Mago();
         Guerrero personaje3 = new Guerrero();
+        System.out.println("Indique puntaje para Constructor");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje1.setPuntaje(puntaje);
+        System.out.println("Indique puntaje para Mago");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje2.setPuntaje(puntaje);
+        System.out.println("Indique puntaje para Guerrero");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje3.setPuntaje(puntaje);
     }
     
 }
